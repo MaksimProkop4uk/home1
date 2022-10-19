d140bc04725a9493db06727d350fe8c6426d8844
Maksim <qjzDyH@mail.ru>
2022-10-19 19:14:20 +0300

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int numA = int.Parse(Console.ReadLine()!);
+if (numA % 2 == 0) Console.WriteLine("Четное");
+                else Console.WriteLine("Нечетное");
