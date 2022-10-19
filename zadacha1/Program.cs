ff3ee789347a3689b9ac081383447ba7f170dc6c
Maksim <qjzDyH@mail.ru>
2022-10-19 19:11:37 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число");
+int numB = int.Parse(Console.ReadLine()!);
+ int max = numA;
+ 
+ if (numA>max) max = numA;
+if (numB>max) max = numB;
+Console.Write("max=");
+Console.WriteLine(max);
